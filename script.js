018ed361d437f620b71dbc882cebba50e46069c7
meijiao <meijiao@gmail.com>
2016-12-24 22:31:04 -0800

    logic for width

--- script.js
+++ script.js
@@ -1,69 +1,114 @@
 const events = [ {start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670} ];
 const containerHeight = 720;
+const containerWidth = 600;
 const minutesinDay = 60 * 12;
-var collisions = [];
+let collisions = [];
+let width = [];
+let leftOffSet = [];
 
-var createEvent = (height, top, width) => {
+var createEvent = (height, top, left, units) => {
 
   // Default CSS that's the same for all
   let node = document.createElement("DIV");
-  node.class = "event";
-  node.style.backgroundColor = "pink";
-  node.style.borderStyle = "solid";
-  node.style.borderWidth = "1px";
-  node.style.borderColor = "grey"
+  node.className = "event";
   node.innerHTML = 
   "<span class='event-title'> Sample Item </span> \
   <br><span class='event-location'> Sample Location </span>";
 
   // Customized CSS to position each event
-  node.style.width = 100/width + "%";
+  node.style.width = 100/units - 1 + "%";
   node.style.height = height + "px";
-  node.style.position = "absolute";
   node.style.top = top + "px";
+  node.style.left = 100 + left + "px";
 
-  document.getElementById("days").appendChild(node);
+  document.getElementById("events").appendChild(node);
 }
 
-// get array that tells you how many events are in a certain time period
-// each index corresponds to 30 minutes on the calendar
+/* 
+collisions is an array that tells you which events are in each 30 min slot
+each index of array corresponds to 30 minutes on the calendar
+e.g.[[], [1], [1,2]] ==> nothing at 9am, event 1 at 9.30, event 1 and 2 at 10am
+*/
 
 (function getCollisions () {
 
   for (var i = 0; i < 24; i ++) {
-    collisions.push(0);
+    var time = [];
+    for (var j = 0; j < events.length; j++) {
+      time.push(0);
+    }
+    collisions.push(time);
   }
 
-  events.forEach((event) => {
+  events.forEach((event, id) => {
     let end = event.end;
     let start = event.start;
 
-    while (end > start) {
-      collisions[Math.ceil(end/30) - 1] ++;
-      end = end - 30;
+    while (start <= end) {
+      timeIndex = Math.floor(start/30);
+      collisions[timeIndex][id] = 1;
+      start = start + 30;
     }
+
   });
+  console.log(collisions);
+
+})();
+
+/*
+find width and horizontal position
+
+width 
+- number of units to divide container width by
+- for each period, set width to element/ length of array
+- update as long as new width is smaller
+
+horizontal position ==> pixel offset from left
+- relates to index in array
+*/
+(function getAttributes () {
+
+  for (var i = 0; i < events.length; i++) {
+    width.push(0);
+  }
+
+  collisions.forEach((period) => {
+    // number of events in that period
+    let count = period.reduce((a,b) => {
+      return a + b;
+    })
+
+    // for each event, get max number of events it is sharing a time period with
+    if (count > 1) {
+      period.forEach((event, id) => {
+        if (period[id]) {
+          if (count > width[id]) {
+            width[id] = count;
+          }
+        }
+      })
+    }
+
+  })
+  console.log(width, "width");
 
 })();
 
 var layOutDay = () => {
 
-
-  events.forEach((event) => {
+  events.forEach((event, id) => {
     let height = (event.end - event.start) / minutesinDay * containerHeight;
     let top = event.start / minutesinDay * containerHeight; 
     let end = event.end;
     let start = event.start;
-    let width = 1;
-
-    while (end > start) {
-      let index = Math.ceil(end/30) - 1;
-      if (collisions[index] > width) {
-        width = collisions[index];
-      }
-      end = end - 30;
-    }
-    createEvent(height, top, width);
+    let units = width[id];
+    if (!units) {units = 1};
+    console.log(units, id, 'yo');
+    //let left = containerWidth / leftOffSet[id];
+    // go through collisions. 
+    // Save the largest number of collisions found in event's time period and use that to determine width
+    createEvent(height, top, 0, units);
   });
 }
+
 
