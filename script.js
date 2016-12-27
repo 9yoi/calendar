3a756369edb8fa918195f323e7dc8b4f025edf4b
meijiao <meijiao@gmail.com>
2016-12-26 23:58:18 -0800

    tried to do it without matrix

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const events = [ {start: 10, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670} ];
+//const events = [ {start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670} ];
+// const events = 
+// [ { start: 215, end: 378 },
+//   { start: 600, end: 687 },
+//   { start: 590, end: 600 },
+//   { start: 559, end: 632 },
+//   { start: 54, end: 677 } ];
+
 const containerHeight = 720;
 const containerWidth = 600;
 const minutesinDay = 60 * 12;
-let collisions = [];
 let width = [];
-let leftOffSet = [];
+let offset = [];
+let columns = 1;
 
 // append one event to calendar
 var createEvent = (height, top, left, units) => {
@@ -16,105 +23,71 @@ var createEvent = (height, top, left, units) => {
   <br><span class='location'> Sample Location </span>";
 
   // Customized CSS to position each event
-  node.style.width = (containerWidth/units) + "px";
+  node.style.width = (containerWidth/columns) * units + "px";
   node.style.height = height + "px";
   node.style.top = top + "px";
-  node.style.left = 100 + left + "px";
+  node.style.left = 100 + 10 + left + "px";
 
   document.getElementById("events").appendChild(node);
 }
 
-/* 
-collisions is an array that tells you which events are in each 30 min slot
-- each first level of array corresponds to a 30 minute slot on the calendar 
-  - [[0 - 30mins], [ 30 - 60mins], ...]
-- next level of array tells you which event is present and the horizontal order
-  - [0,0,1,2] 
-  ==> event 1 is not present, event 2 is not present, event 3 is at order 1, event 4 is at order 2
-*/
+// this function lays out the events which are overlapping in a range. 
+// it is used to get the width of an event
+function getCollisions (events) {
 
-function getCollisions () {
-
-  //resets storage
-  collisions = [];
-
-  for (var i = 0; i < 24; i ++) {
-    var time = [];
-    for (var j = 0; j < events.length; j++) {
-      time.push(0);
-    }
-    collisions.push(time);
-  }
-
-  events.forEach((event, id) => {
-    let end = event.end;
-    let start = event.start;
-    let order = 1;
-
-    while (start < end) {
-      timeIndex = Math.floor(start/30);
-
-      while (order < events.length) {
-        if (collisions[timeIndex].indexOf(order) === -1) {
-          break;
-        }
-        order ++;
-      }
-
-      collisions[timeIndex][id] = order;
-      start = start + 30;
-    }
-
-    collisions[Math.floor((end-1)/30)][id] = order;
-  });
-};
-
-/*
-find width and horizontal position
-
-width - number of units to divide container width by
-horizontal position - pixel offset from left
-
-*/
-function getAttributes () {
-
-  //resets storage
+  //resets variable
   width = [];
-  leftOffSet = [];
+  offset = [];
 
+  let visited = [];
+
+  // check each event against what has been visited
   for (var i = 0; i < events.length; i++) {
-    width.push(0);
-    leftOffSet.push(0);
-  }
+    //first event
+    var event = events[i];
+    var overlap = false;
 
-  collisions.forEach((period) => {
-
-    // number of events in that period
-    let count = period.reduce((a,b) => {
-      return b ? a + 1 : a;
-    })
-
-    if (count > 1) {
-      period.forEach((event, id) => {
-        // max number of events it is sharing a time period with determines width
-        if (period[id]) {
-          if (count > width[id]) {
-            width[id] = count;
-          }
-        }
-
-        if (period[id] && !leftOffSet[id]) {
-          leftOffSet[id] = period[id];
-        }
-      })
+    if (visited.length === 0) {
+      visited.push(event);
+      width.push(1);
+      offset.push(1)
+      continue;
     }
-  });
-};
+    // check event against all events previously visited
+    // if conflict, increment columns
+    // for all not in conflict, match column in width
+    // 
+    for (var j = 0; j < visited.length; j++) {
+      var visit = visited[j];
 
-var layOutDay = () => {
+      if (event.end > visit.start && event.end < visit.end || 
+          event.start > visit.start && event.start < visit.end ||
+          event.start > visit.start && event.end < visit.end ||
+          visit.start > event.start && visit.end < event.end) {     
+        columns ++
+        width.push(1);
+        offset.push(columns);
+        overlap = !overlap;
+        break;
+      } 
+    }
 
-  getCollisions();
-  getAttributes();
+    if (!overlap) {
+      width.push(1);
+      offset.push(1);
+    }
+
+    visited.push(event);
+  }
+  console.log(visited, columns, width, offset);
+} 
+//test
+console.log(getCollisions(events));
+
+
+var layOutDay = (events) => {
+
+  getCollisions(events);
 
   events.forEach((event, id) => {
     let height = (event.end - event.start) / minutesinDay * containerHeight;
@@ -122,9 +95,7 @@ var layOutDay = () => {
     let end = event.end;
     let start = event.start;
     let units = width[id];
-    if (!units) {units = 1};
-    let left = (containerWidth / width[id]) * (leftOffSet[id] - 1) + 10;
-    if (!left || left < 0) {left = 10};
+    let left = (containerWidth / columns) * (offset[id] - 1) ;
     createEvent(height, top, left, units);
   });
 }
