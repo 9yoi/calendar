9f4592bab79a33639b5d27d3c9078a07d15553b4
meijiao <meijiao@gmail.com>
2016-12-29 01:22:24 -0800

    implementing offset function

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ const events = [ {start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end
 const containerHeight = 720;
 const containerWidth = 600;
 const minutesinDay = 60 * 12;
+let widths = [];
+let columns = 1;
+let offset = [];
 
 // helper function to append one event to DOM
-var createEvent = (height, top, left, units) => {
+var createEvent = (height, top, left, width) => {
 
   let node = document.createElement("DIV");
   node.className = "event";
@@ -22,7 +25,7 @@ var createEvent = (height, top, left, units) => {
   <br><span class='location'> Sample Location </span>";
 
   // Customized CSS to position each event
-  node.style.width = (containerWidth/columns) * units + "px";
+  node.style.width = width + "px";
   node.style.height = height + "px";
   node.style.top = top + "px";
   node.style.left = 100 + 10 + left + "px";
@@ -30,17 +33,7 @@ var createEvent = (height, top, left, units) => {
   document.getElementById("events").appendChild(node);
 }
 
-/*
-Find out which events are in conflict with each other
-Returns an array of array where 
-- Index references eventId
-- Numbers in array references the events it is in conflict with
-e.g.[[1,2],[1,0]]
-=> Event 0 has conflict with event 1 and 2 
-=> Event 1 has conflict with Event 0 and 1
-
-Initial width is container size / max number of conflicts
-
+/* helper function to find collisions
 Visual of possible collision types                            
                                                                                          
                   ***                                           
@@ -54,50 +47,115 @@ Visual of possible collision types
                   ***  
 */
 
+var checkCollisions = (event, otherEvent) => {
+  if (event.start >= otherEvent.start && event.start <= otherEvent.end ||
+        event.end >= otherEvent.start && event.end <= otherEvent.end ||
+        event.start <= otherEvent.start && event.end >= otherEvent.end ||
+        event.start >= otherEvent.start && event.end <= otherEvent.end) {
+    return true;
+  } 
+  return false;
+}
+
+/*
+Find out which events are in conflict with each other
+Returns an array of array where 
+- Index references eventId
+- Numbers in array references the events it is in conflict with
+e.g.[[1,2],[1,0]]
+=> Event 0 has conflict with event 1 and 2 
+=> Event 1 has conflict with Event 0 and 1
+
+Initial width is container size / max number of conflicts
+
+*/
+
+
 var getWidth = events => {
 
   let collisions = [];
-  let width = [];
   // check each event against every other event
   events.forEach((event, id1) => {   
     collisions.push([]);
 
     events.forEach((otherEvent, id2) => {
-
-      if (event.start >= otherEvent.start && event.start <= otherEvent.end ||
-        event.end >= otherEvent.start && event.end <= otherEvent.end ||
-        event.start <= otherEvent.start && event.end >= otherEvent.end ||
-        event.start >= otherEvent.start && event.end <= otherEvent.end) {
+      if (checkCollisions(event, otherEvent)) {
         collisions[id1].push(id2);
       }
     })
   })
 
-  collisions.forEach((event) => width.push(containerWidth / event.length))
-  return width;
+  // get each container width and columns needed to fit all events
+  collisions.forEach((event) => {
+    widths.push(containerWidth / event.length)
+    if (event.length > columns) {
+      columns = event.length;
+    }
+  })
+
+  return widths;
 } 
 
-// always slot item into position one. 
+// always slot item into column one. 
 // if there is already something there, + width of the last item in conflict with
-var findOffset  = () => {
-  //reset offset
-  let 
-  offset = [];
+var findOffset = (events) => {
 
+  let slotted = [];
+
+  events.forEach((event, id) => {
+    //first event
+    if (slotted.length === 0) {
+      slotted.push([event]);
+    }
+
+    // check event against whatever has been added to slotted
+    for (var i = 0; i < slotted.length; i++) {
+      let column = slotted[i];
+      let conflict = false;
+      // if collision within a slot, move to next slot by jumping out of inner loop
+      for (var j = 0; j < column.length; j++) {
+        let period = column[j];
+        if(checkCollisions(event, period)) {
+          conflict = !conflict;
+          break;
+        }
+      }
+
+      // if no collision for that slot, add event and terminate
+      // event should be added for all columns according to width
+      // e.g. if each column is 200px, a 400px event has to be added to 2 columns
+      if (!conflict) {
+        let width = widths[id];
+        let column = i;
+        offset[id] = containerWidth/columns * (i);
+        while (width) {
+          slotted[column] ? slotted[column].push(event) : slotted[column] = [event];
+          width = width - containerWidth/columns;
+          column ++; 
+        }
+        break;
+      }
+    }
+  });
+
+  return slotted;
+    
 }
 
 var layOutDay = (events) => {
 
-  let width = getWidth(events);
+  getWidth(events);
+  findOffset(events);
 
   events.forEach((event, id) => {
     let height = (event.end - event.start) / minutesinDay * containerHeight;
     let top = event.start / minutesinDay * containerHeight; 
     let end = event.end;
     let start = event.start;
-    let units = width[id];
-    let left = (containerWidth / columns) * (offset[id] - 1) ;
-    createEvent(height, top, left, units);
+    let width = widths[id];
+    let left = offset[id];
+    console.log(event, height, top, left, width);
+    createEvent(height, top, left, width);
   });
 }
 
